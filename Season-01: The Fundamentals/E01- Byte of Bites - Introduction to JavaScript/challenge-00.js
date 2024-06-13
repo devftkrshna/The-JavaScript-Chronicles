eea81e75ccfc515cb2a72b41f2b52e7df6d6668b
Vaibhav Sharma <vaibhavkhandal445@gmail.com>
2024-06-13 19:43:49 +0530

    Create EP01

---
+++ Season-01: The Fundamentals/E01- Byte of Bites - Introduction to JavaScript/challenge-00.js
@@ -0,0 +1,5 @@
+let fname="Vaibhav";
+let whatDoYouWannaBecomeInYourLife="programmer";
+const gender='Male';
+let twitterHandle='devftkrshna';
+console.log('My name is '+fname+" and I want to become a "+ whatDoYouWannaBecomeInYourLife +"\nGender: "+gender+"\nTwitter: "+twitterHandle);
