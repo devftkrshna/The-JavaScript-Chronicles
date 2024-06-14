e8edf43e421caf178582407ff3b0c73f007ceab4
Vaibhav Sharma <vaibhavkhandal445@gmail.com>
2024-06-14 08:02:22 +0530

    commit on 14/06/2023

---
+++ Season-01- The Fundamentals/EP01 - Byte of Bites - Introduction to JavaScript/Unary Operators.js
@@ -0,0 +1,10 @@
+//'typeof' --gives the type of the variable 
+let type =typeof 6; ///type is number
+
+//'++' -- Increament; increase by 1
+let x=5;
+x++; //x is 6 now
+
+// '--' Decreamnet; decrease by 1
+let y=5;
+y--; //y is 4 now
