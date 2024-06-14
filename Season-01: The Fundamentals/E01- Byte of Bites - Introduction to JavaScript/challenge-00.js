9380fac5447ab81066f823642c8511399b7b54b0
Vaibhav Sharma <vaibhavkhandal445@gmail.com>
2024-06-14 07:14:05 +0530

    empty repository

--- Season-01: The Fundamentals/E01- Byte of Bites - Introduction to JavaScript/challenge-00.js
+++
@@ -1,5 +0,0 @@
-let fname="Vaibhav";
-let whatDoYouWannaBecomeInYourLife="programmer";
-const gender='Male';
-let twitterHandle='devftkrshna';
-console.log('My name is '+fname+" and I want to become a "+ whatDoYouWannaBecomeInYourLife +"\nGender: "+gender+"\nTwitter: "+twitterHandle);
