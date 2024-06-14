9544f10cbb1faaaf0d514e9068ed69cd8bc08735
Vaibhav Sharma <vaibhavkhandal445@gmail.com>
2024-06-14 07:20:10 +0530

    create ep01

---
+++ Season-01- The Fundamentals/EP01 - Byte of Bites - Introduction to JavaScript/Arithematic Operators.js
@@ -0,0 +1,19 @@
+// 1. Arithmetic Operators:
+//- `+` : Addition
+let sum = 5 + 3; // sum is 8
+//- `-` : Subtraction
+let difference = 10 - 4; // difference is 6
+//- `*` : Multiplication
+let product = 2 * 3; // product is 
+//- `/` : Division
+let quotient = 15 / 3; // quotient is 5
+//- `%` : Modulus (Remainder)
+let remainder = 7 % 2; // remainder is 1
+//- `**` : Exponentiation (ES6)
+let power = 2 ** 3; // power is 8
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+console.log(remainder);
+console.log(power);
