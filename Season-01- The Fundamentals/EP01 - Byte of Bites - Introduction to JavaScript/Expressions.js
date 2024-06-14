43e2e6245ca2fa2b35ac1d2792ebc3c9cfaeeee9
Vaibhav Sharma <vaibhavkhandal445@gmail.com>
2024-06-14 08:16:12 +0530

    intial commit

---
+++ Season-01- The Fundamentals/EP01 - Byte of Bites - Introduction to JavaScript/Expressions.js
@@ -0,0 +1,12 @@
+//Arithematic Expression
+let total = (5 + 3) ** 2 * 2; //total is 128
+console.log(total);
+
+//String Expression
+let greeting = "Hello" + " " + "World"; //greeting is Hello World
+
+//Logical Expression
+let isAdult = age >= 18 && age <= 65; // isAdult is true if age is between 18 and 65
+
+//Comparision Expression
+let isEqual = 5 == 5; //isEqual is true
