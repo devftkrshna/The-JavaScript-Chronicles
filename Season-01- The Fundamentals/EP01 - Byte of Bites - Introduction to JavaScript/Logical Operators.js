d1aa2c2c4018e2d04289659c64a1656c09987eaf
Vaibhav Sharma <vaibhavkhandal445@gmail.com>
2024-06-14 08:01:52 +0530

    create logical operators.js

---
+++ Season-01- The Fundamentals/EP01 - Byte of Bites - Introduction to JavaScript/Logical Operators.js
@@ -0,0 +1,13 @@
+// '&&' And Operator; true if and only if both the booleans(operands) are true
+let andResult = (true&&false); //andResult is false
+let andResult_= (true&&true); //andResult_ is true
+
+// '||' Or Operator; False if and only is both of the booleans(operands) are false
+let orResult =(true||false); //orResult is true
+let orResult_ =(false||false); //orResult is false
+
+// '!' Not Operator, changee the boolean
+let notResult = !true; //false
+let notResult_ =!false; //true
+
+console.log(andResult);
